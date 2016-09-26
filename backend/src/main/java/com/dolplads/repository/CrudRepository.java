856149317b0c731265170f850cbc6388e24ff92c
dolps <dolplads@gmail.com>
2016-09-26 18:49:02 +0200

    added repo

---
+++ backend/src/main/java/com/dolplads/repository/CrudRepository.java
@@ -0,0 +1,7 @@
+package com.dolplads.repository;
+
+/**
+ * Created by dolplads on 26/09/16.
+ */
+public class CrudRepository<T> {
+}
--- backend/src/main/java/com/dolplads/service/UserService.java
+++ backend/src/main/java/com/dolplads/service/UserService.java
@@ -1,8 +1,11 @@
 package com.dolplads.service;
 
+import com.dolplads.model.User;
+import com.dolplads.repository.CrudRepository;
+
 /**
  * Created by dolplads on 26/09/16.
  */
-public class UserService {
+public class UserService extends CrudRepository<User> {
 
 }
